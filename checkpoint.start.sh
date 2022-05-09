16b390050716cf7593949a3da376e75afeb1ab27
EvoxllEco <80033183+EvoxllEco@users.noreply.github.com>
2022-05-10 00:54:15 +0200

    Update checkpoint.start.sh

--- checkpoint.start.sh
+++ checkpoint.start.sh
@@ -1,1 +1,3 @@
+Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping Farmhouse?Scenario=Scenario_Farmhouse_Checkpoint_Security?Game=CheckpointHardcore?MaxPlayers=6 -GameStatsToken=(TokenHere) -Port=27102 -QueryPort=27131 -log -MapCycle=OtherMapCycle -AdminList=Admins -mutators=AllYouCanEat,Hardcore -hostname="=[ AWSM ]= Survival | Hardcore | COOP | DE"
+
 
