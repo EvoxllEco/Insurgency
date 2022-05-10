b855fa75dc1c3e1a45b2d5d39627fbc07d88ad46
EvoxllEco <80033183+EvoxllEco@users.noreply.github.com>
2022-05-10 02:14:08 +0200

    Update survival.start.sh

--- survival.start.sh
+++ survival.start.sh
@@ -1,2 +1,2 @@
-Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping Farmhouse?Scenario=Scenario_Farmhouse_Survival?Game=Survival?MaxPlayers=6 -GameStatsToken=(TokenHere) -Port=27102 -QueryPort=27131 -log -MapCycle=OtherMapCycle -AdminList=Admins -mutators=AllYouCanEat,Hardcore -hostname="=[ AWSM ]= Survival | Hardcore | COOP | DE"
+Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping Farmhouse?Scenario=Scenario_Farmhouse_Survival?Game=Survival?MaxPlayers=6 -GameStatsToken=91CDD4DBBE1F404CBCF6C78232AE2152 -Port=27102 -QueryPort=27131 -log -MapCycle=OtherMapCycle -AdminList=Admins -mutators=AllYouCanEat,Hardcore -hostname="=[ AWSM ]= Survival | Hardcore | COOP | DE"
 
